16b9e2b0ab2e259a0e23baf84c272ea16ca4d83c
voje <kristjan.voje@gmail.com>
2015-02-23 02:49:52 +0100

    pills dogodki se odzivajo na klike

--- js/script.js
+++ js/script.js
@@ -8,10 +8,30 @@ $(document).ready(function(){
 	$.get("nav.html", function(data1){
 		$("#my_nav").html(data1);
 	});
+	loadContent("./pages/dogodki.html");
 });
 
 function loadContent(content){
 	$.get(content, function(data){
 		$("#my_content").html(data);
 	});
+}
+
+function loadDogodki(){
+	$.get("./datoteke/dogodki/dogodki.json", function(dog){
+		console.log("loaded");
+		console.log(dog);
+		for(i in dog["dogodki"]){
+			var datum = dog["dogodki"][i]["datum"];
+			var active = "";
+			if(i == dog["dogodki"].length-1){
+				active = "class = 'active'";
+			}
+			$("#d_dogodki ul").prepend("<li role='presentation'" + active + "><a href='#'>" + datum + "</a></li>");
+		}
+	}, "json");
+}
+
+function izberiDogodek(){
+
 }
