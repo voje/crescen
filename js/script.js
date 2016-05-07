4203fba3d250afe775024b591581c394d44ae8d1
voje <kristjan.voje@gmail.com>
2016-05-07 03:06:23 +0200

    todo: navbar, scroll down button, content

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-$(document).ready(function(){
-	console.log("hello");
-});
+var navbar = $("#navbar");
+var hero = $("#hero");
+var hero_text = $("#hero_text");
 
 function scroll_callback(){
 	var y_pos, jumbotron, header_text;
 	y_pos = window.pageYOffset;
-	$("#hero").css("top", y_pos * 0.7);
-	$("#hero_text").css("opacity", 30/y_pos);
-	$("#hero_text").css("top", -y_pos * 0.2);
+	if(y_pos > hero.height()){
+		navbar.fadeIn("slow");
+	}else{
+		navbar.fadeOut("slow");
+	}
+	hero.css("top", y_pos * 0.7);
+	hero_text.css("opacity", 30/y_pos);
+	hero_text.css("top", -y_pos * 0.2);
 }
+
+$(document).ready(function(){
+	console.log("greetings!");
+});
 
 window.addEventListener("scroll", scroll_callback);
