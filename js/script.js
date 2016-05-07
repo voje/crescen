aa7fe69901f77b7a58163a6121f802771389226b
voje <kristjan.voje@gmail.com>
2016-05-07 19:51:08 +0200

    working on content

--- js/script.js
+++ js/script.js
@@ -1,22 +1,62 @@
-var navbar = $("#navbar");
-var hero = $("#hero");
-var hero_text = $("#hero_text");
+var select = {
+	navbar: $("#navbar"),
+	hero: $("#hero"),
+	hero_text: $("#hero_text"),
+	recent_event: $("#recent_event")	
+};
+
+var events_data = {};
+
+function display_event(id){
+	event = events_data[id];
+	//display header, video?, place, time?, poster?, commentary
+	select.recent_event.find("a").append("<h2>"+ event.ime +"</h2>");	
+
+	if(event.opis != "")
+		select.recent_event.append("<p>"+ event.opis +"</p>");	
+
+	if(event.videos.length != 0)
+		for(i in event.videos){
+			select.recent_event.append("<figure><iframe src='"+ event.videos[i] +"'/></figure>");
+		}
+
+	if(event.kraj != "")
+		select.recent_event.append("<p>"+ event.kraj +"</p>");	
+
+	if(event.datum != "")
+		select.recent_event.append("<p>"+ event.datum +"</p>");	
+
+	if(event.img_plakat != "")
+		select.recent_event.append("<figure><img src='"+ event.img_plakat +"'/>");	
+
+	if(event.besedilo != "")
+		select.recent_event.append("<p>"+ event.besedilo +"</p>");	
+}
+
+function load_events_data(data){
+	events_data = data;
+	var keys = Object.keys(events_data);	
+	var last_event_id = keys[keys.length - 1];
+	//first run: display the last event on homepage
+	display_event(last_event_id);
+}
 
 function scroll_callback(){
 	var y_pos, jumbotron, header_text;
 	y_pos = window.pageYOffset;
-	if(y_pos > hero.height()){
-		navbar.fadeIn("slow");
+	if(y_pos > select.hero.height()){
+		select.navbar.fadeIn("slow");
 	}else{
-		navbar.fadeOut("slow");
+		select.navbar.fadeOut("slow");
 	}
-	hero.css("top", y_pos * 0.7);
-	hero_text.css("opacity", 30/y_pos);
-	hero_text.css("top", -y_pos * 0.2);
+	select.hero.css("top", y_pos * 0.7);
+	select.hero_text.css("opacity", 30/y_pos);
+	select.hero_text.css("top", -y_pos * 0.2);
 }
 
 $(document).ready(function(){
 	console.log("greetings!");
+	$.getJSON("./data/dogodki.json", load_events_data);
 });
 
 window.addEventListener("scroll", scroll_callback);
